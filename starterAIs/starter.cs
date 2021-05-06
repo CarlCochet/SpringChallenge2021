708ce7ec6cdcd630b4596f1d543becf52299dc90
Carl <carl.cochet@gmail.com>
2021-05-06 20:21:32 +0200

    Update starter.cs

--- starterAIs/starter.cs
+++ starterAIs/starter.cs
@@ -5,42 +5,182 @@ using System.Text;
 using System.Collections;
 using System.Collections.Generic;
 
+
+class Player
+{
+    static void Main(string[] args)
+    {
+        string[] inputs;
+        int numberOfCells = int.Parse(Console.ReadLine());
+
+        Cell[] cells = new Cell[37];
+
+        for (int i = 0; i < numberOfCells; i++)
+        {
+            inputs = Console.ReadLine().Split(' ');
+            int index = int.Parse(inputs[0]); // 0 is the center cell, the next cells spiral outwards
+            int richness = int.Parse(inputs[1]); // 0 if the cell is unusable, 1-3 for usable cells
+            int neigh0 = int.Parse(inputs[2]); // the index of the neighbouring cell for each direction
+            int neigh1 = int.Parse(inputs[3]);
+            int neigh2 = int.Parse(inputs[4]);
+            int neigh3 = int.Parse(inputs[5]);
+            int neigh4 = int.Parse(inputs[6]);
+            int neigh5 = int.Parse(inputs[7]);
+
+            int[] neighbors = {neigh0, neigh1, neigh2, neigh3, neigh4, neigh5};
+            cells[i] = new Cell(index, richness, neighbors);
+        }
+
+        while (true)
+        {
+            int day = int.Parse(Console.ReadLine()); // the game lasts 24 days: 0-23
+            int nutrients = int.Parse(Console.ReadLine()); // the base score you gain from the next COMPLETE action
+            inputs = Console.ReadLine().Split(' ');
+            int sun = int.Parse(inputs[0]); // your sun points
+            int score = int.Parse(inputs[1]); // your current score
+            inputs = Console.ReadLine().Split(' ');
+            int oppSun = int.Parse(inputs[0]); // opponent's sun points
+            int oppScore = int.Parse(inputs[1]); // opponent's score
+            bool oppIsWaiting = inputs[2] != "0"; // whether your opponent is asleep until the next day
+            int numberOfTrees = int.Parse(Console.ReadLine()); // the current amount of trees
+            
+            List<Tree> trees = new List<Tree>();
+            List<Tree> sorted = new List<Tree>();
+
+            for (int i = 0; i < numberOfTrees; i++)
+            {
+                inputs = Console.ReadLine().Split(' ');
+                int cellIndex = int.Parse(inputs[0]); // location of this tree
+                int size = int.Parse(inputs[1]); // size of this tree: 0-3
+                bool isMine = inputs[2] != "0"; // 1 if this is your tree
+                bool isDormant = inputs[3] != "0"; // 1 if this tree is dormant
+
+                Tree tree = new Tree(cells[cellIndex], size, isMine, isDormant);
+                trees.Add(tree);
+
+                if (isMine) 
+                {
+                    int sort_index = 0;
+                    for (int k = 0; k < sorted.Count; k++)
+                    {
+                        if (tree.size > sorted[k].size)
+                        {
+                            sort_index = k;
+                            break;
+                        }
+                    }
+                    sorted.Insert(sort_index, tree);
+                }
+            }
+
+            int numberOfPossibleMoves = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numberOfPossibleMoves; i++)
+            {
+                string possibleMove = Console.ReadLine();
+            }
+
+            Spirit[] players = {new Spirit(sun, score, false), new Spirit(oppSun, oppScore, oppIsWaiting)};
+            GameState state = new GameState(day, nutrients, numberOfTrees, cells, trees, players);
+
+            // To debug: Console.Error.WriteLine("Debug messages...");
+
+            // GROW cellIdx | SEED sourceIdx targetIdx | COMPLETE cellIdx | WAIT <message>
+
+            Console.Error.WriteLine("Sun: {0} | My trees: {1}", sun, sorted.Count);
+
+            if (sun > 4 && sorted.Count > 0)
+            {
+                Console.WriteLine("COMPLETE {0}", sorted[0].cell.index);
+            } 
+            else
+            {
+                Console.WriteLine("WAIT");
+            }
+        }
+    }
+}
+
+
+class GameState 
+{
+    public int day;
+    public int nutrients;
+    public int numberOfTrees;
+    public Cell[] map;
+    public List<Tree> trees;
+    public Spirit[] players;
+    
+    public GameState(int day, int nutrients, int numberOfTrees, Cell[] map, List<Tree> trees, Spirit[] players)
+    {
+        this.day = day;
+        this.nutrients = nutrients;
+        this.numberOfTrees = numberOfTrees;
+        this.map = map;
+        this.trees = trees;
+        this.players = players;
+    }
+
+    public List<Action> PossibleMoves()
+    {
+        return new List<Action>();
+    }
+}
+
 class Cell
 {
     public int index;
-    public int richess;
-    public int[] neighbours;
+    public int richness;
+    public int[] neighbors;
 
-    public Cell(int index, int richess, int[] neighbours)
+    public Cell(int index, int richness, int[] neighbors)
     {
         this.index = index;
-        this.richess = richess;
-        this.neighbours = neighbours;
+        this.richness = richness;
+        this.neighbors = neighbors;
     }
 }
 
 class Tree
 {
-    public int cellIndex;
+    public Cell cell;
     public int size;
     public bool isMine;
     public bool isDormant;
 
-    public Tree(int cellIndex, int size, bool isMine, bool isDormant)
+    public Tree(Cell cell, int size, bool isMine, bool isDormant)
     {
-        this.cellIndex = cellIndex;
+        this.cell = cell;
         this.size = size;
         this.isMine = isMine;
         this.isDormant = isDormant;
+    }
+
+    public int GetScore(int nutrients)
+    {
+        return size == 3 ? nutrients + (2 * (cell.richness - 1)) : 0;
+    }
+}
+
+class Spirit
+{
+    public int sun;
+    public int score;
+    public bool isWaiting;
+
+    public Spirit(int sun, int score, bool isWaiting)
+    {
+        this.sun = sun;
+        this.score = score;
+        this.isWaiting = isWaiting;
     }
 }
 
 class Action
 {
-    public const string WAIT = "WAIT";
-    public const string SEED = "SEED";
-    public const string GROW = "GROW";
-    public const string COMPLETE = "COMPLETE";
+    const string WAIT = "WAIT";
+    const string SEED = "SEED";
+    const string GROW = "GROW";
+    const string COMPLETE = "COMPLETE";
 
     public static Action Parse(string action)
     {
@@ -58,9 +198,9 @@ class Action
         }
     }
 
-    public string type;
-    public int targetCellIdx;
-    public int sourceCellIdx;
+    string type;
+    int targetCellIdx;
+    int sourceCellIdx;
 
     public Action(string type, int sourceCellIdx, int targetCellIdx)
     {
@@ -90,95 +230,5 @@ class Action
             return string.Format("{0} {1} {2}", SEED, sourceCellIdx, targetCellIdx);
         }
         return string.Format("{0} {1}", type, targetCellIdx);
-    }
-}
-
-class Game
-{
-    public int day;
-    public int nutrients;
-    public List<Cell> board;
-    public List<Action> possibleActions;
-    public List<Tree> trees;
-    public int mySun, opponentSun;
-    public int myScore, opponentScore;
-    public bool opponentIsWaiting;
-
-    public Game()
-    {
-        board = new List<Cell>();
-        possibleActions = new List<Action>();
-        trees = new List<Tree>();
-    }
-
-    public Action GetNextAction()
-    {
-        // TODO: write your algorithm here
-        return possibleActions.First();
-    }
-}
-
-class Player
-{
-    static void Main(string[] args)
-    {
-        string[] inputs;
-
-        Game game = new Game();
-
-        int numberOfCells = int.Parse(Console.ReadLine()); // 37
-        for (int i = 0; i < numberOfCells; i++)
-        {
-            inputs = Console.ReadLine().Split(' ');
-            int index = int.Parse(inputs[0]); // 0 is the center cell, the next cells spiral outwards
-            int richness = int.Parse(inputs[1]); // 0 if the cell is unusable, 1-3 for usable cells
-            int neigh0 = int.Parse(inputs[2]); // the index of the neighbouring cell for each direction
-            int neigh1 = int.Parse(inputs[3]);
-            int neigh2 = int.Parse(inputs[4]);
-            int neigh3 = int.Parse(inputs[5]);
-            int neigh4 = int.Parse(inputs[6]);
-            int neigh5 = int.Parse(inputs[7]);
-            int[] neighs = new int[] { neigh0, neigh1, neigh2, neigh3, neigh4, neigh5 };
-            Cell cell = new Cell(index, richness, neighs);
-            game.board.Add(cell);
-        }
-
-        // game loop
-        while (true)
-        {
-            game.day = int.Parse(Console.ReadLine()); // the game lasts 24 days: 0-23
-            game.nutrients = int.Parse(Console.ReadLine()); // the base score you gain from the next COMPLETE action
-            inputs = Console.ReadLine().Split(' ');
-            game.mySun = int.Parse(inputs[0]); // your sun points
-            game.myScore = int.Parse(inputs[1]); // your current score
-            inputs = Console.ReadLine().Split(' ');
-            game.opponentSun = int.Parse(inputs[0]); // opponent's sun points
-            game.opponentScore = int.Parse(inputs[1]); // opponent's score
-            game.opponentIsWaiting = inputs[2] != "0"; // whether your opponent is asleep until the next day
-
-            game.trees.Clear();
-            int numberOfTrees = int.Parse(Console.ReadLine()); // the current amount of trees
-            for (int i = 0; i < numberOfTrees; i++)
-            {
-                inputs = Console.ReadLine().Split(' ');
-                int cellIndex = int.Parse(inputs[0]); // location of this tree
-                int size = int.Parse(inputs[1]); // size of this tree: 0-3
-                bool isMine = inputs[2] != "0"; // 1 if this is your tree
-                bool isDormant = inputs[3] != "0"; // 1 if this tree is dormant
-                Tree tree = new Tree(cellIndex, size, isMine, isDormant);
-                game.trees.Add(tree);
-            }
-
-            game.possibleActions.Clear();
-            int numberOfPossibleMoves = int.Parse(Console.ReadLine());
-            for (int i = 0; i < numberOfPossibleMoves; i++)
-            {
-                string possibleMove = Console.ReadLine();
-                game.possibleActions.Add(Action.Parse(possibleMove));
-            }
-
-            Action action = game.GetNextAction();
-            Console.WriteLine(action);
-        }
     }
 }
